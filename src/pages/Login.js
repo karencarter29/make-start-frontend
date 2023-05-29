561461fdb8e2170961e1f3faf449a9a3409d48ad
Karyna <karen.carter.29.12@gmail.com>
2023-05-29 20:52:06 +0300

    added logic for registration

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,8 +1,16 @@
-import React from 'react';
+import React, { useState } from 'react';
+
 import Logo from '../components/Logo';
 import '../assets/styles/Login.css';
 
 function Login() {
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+
+    function onSubmit() {
+        // sendData();
+    }
+
     return(
         <>
             <div className='login-container'>
@@ -12,11 +20,23 @@ function Login() {
                     <div className='login-title'>ВХІД</div>
                     <form>
                         <label for='login'>Логін</label><br></br>
-                        <input type='text' id='login' name='login'></input><br></br>
+                        <input 
+                            type='text' 
+                            id='login' 
+                            name='login'
+                            value={username}
+                            onChange={(e) => setUsername(e.target.value)}
+                        /><br></br>
                         <label for='password'>Пароль</label><br></br>
-                        <input type='text' id='password' name='password'></input><br></br>
+                        <input 
+                            type='text' 
+                            id='password' 
+                            name='password'
+                            value={password}
+                            onChange={(e) => setPassword(e.target.value)}
+                        /><br></br>
                     </form>
-                    <button className='login-button'>Увійти</button>
+                    <button className='login-button' onClick={onSubmit}>Увійти</button>
                     <div className='login-no-profile'>Нема акаунту? Зареєструватись.</div>
                     <div className='login-copyright'>MAKESTART © COPYRIGHT 2023</div>
                 </div>
