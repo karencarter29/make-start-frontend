1cb0cf493d1354d47e2b6bfd578ddff5b3a2ee06
Karyna <karen.carter.29.12@gmail.com>
2023-06-04 19:56:07 +0300

    updated main page to display 10 random project

--- src/components/ProjectBox.js
+++ src/components/ProjectBox.js
@@ -3,8 +3,15 @@ import '../assets/styles/ProjectBox.css'
 function ProjectBox(props) {
     return (
         <div className="box">
-        <div className="form">{props.projectName}</div>
-    </div>
+            <div className="form">{props.project.projectName}</div>
+            <div className='box-content'>
+                <div className='description'>{props.project.description}</div>
+                <div className='members'>Team: {props.project.countOfTeammates}, Open positions: {props.project.countOfProposition}</div>
+            </div>
+            <div className='box-left-content'>
+                <div className='price'>{props.project.price}$</div>
+            </div>
+        </div>
     );
 }
 
